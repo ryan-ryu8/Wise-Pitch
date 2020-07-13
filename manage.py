8647e1048a2d182f413a24711688abe0e479cedb
Munge Ryan <mungeryan@gmail.com>
2020-07-13 12:08:20 +0300

    importing migrate from flask migrate

--- manage.py
+++ manage.py
@@ -1,6 +1,8 @@
-from app import create_app,db
+from app import create_app, db
 from flask_script import Manager, Server
-from app.models import User, Pitch, Category,Vote, Comment
+from app.models import User, Pitch, Category, Vote, Comment
+from flask_migrate import Migrate, MigrateCommand
+
 
 app = create_app('production')
 
