6902708e9b635ad87fca2f4d176f7e4df58df6c6
Munge Ryan <mungeryan@gmail.com>
2020-07-13 10:26:56 +0300

    Adding a radiofield for like n unlike

--- app/main/forms.py
+++ app/main/forms.py
@@ -6,3 +6,14 @@ class UpdateProfile(FlaskForm):
     bio = TextAreaField('Please tell us about yourself.',validators = [Required()])
     submit = SubmitField('Submit')
 
+class PitchForm(FlaskForm):
+    content = TextAreaField('Post your wise-pitch')
+    submit = SubmitField('Submit my pitch')
+
+class CommentForm(FlaskForm):
+    comment = TextAreaField('Comment', validators=[Required()])
+    vote = RadioField('default field arguments', choices=[('1', 'Like'), ('1', 'Unlike')])
+
+class CategoryForm(FlaskForm):
+    name = TextAreaField('Category')
+    submit = SubmitField()
