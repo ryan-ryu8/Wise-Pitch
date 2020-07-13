bf54c7e295628673a3e274baa84e72f42f1cd2cd
Munge Ryan <mungeryan@gmail.com>
2020-07-13 11:18:11 +0300

    importing server from flask_scripts

--- manage.py
+++ manage.py
@@ -1,11 +1,26 @@
 from app import create_app,db
-from app.models import User
+from flask_script import Manager, Server
+from app.models import User, Pitch, Category,Vote, Comment
 
+app = create_app('production')
 
+manager = Manager(app)
+migrate = Migrate(app, db)
+
+manager.add_command('server',Server)
+manager.add_command('db', MigrateCommand)
+
+manager.add_command('server',Server)
+@manager.command
+def test():
+    """Run the unit tests."""
+    import unittest
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
 
 @manager.shell
 def make_shell_context():
-    return dict(app = app,db = db,User = User)
+    return dict(app = app,db = db,User = User, Pitch = Pitch, Category = Category, Vote = Vote, Comment = Comment)
 
 if __name__ == '__main__':
     manager.run()
